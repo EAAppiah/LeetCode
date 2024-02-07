add2e17d66df9232387dbb02b902e9196d7c6dee
Captain Hook <Tooriskygh@gmail.com>
2024-02-07 12:24:30 +0000

    initial commit

---
+++ ContainsDuplicate.js
@@ -0,0 +1,35 @@
+// QUESTION
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// ANSWER
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  let tracker = new Set();
+  for (let num of nums) {
+      if (tracker.has(num)) {
+          return true;
+      }
+      tracker.add(num);
+  }
+  return false;
+};
